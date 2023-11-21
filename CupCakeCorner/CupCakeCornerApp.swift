88b9f1590a6ef7b3eae705b3a7d9a7732a20616b
A-f-Adib <109586107+A-f-Adib@users.noreply.github.com>
2023-11-22 01:42:39 +0600

    Initial Commit

---
+++ CupCakeCorner/CupCakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupCakeCornerApp.swift
+//  CupCakeCorner
+//
+//  Created by A.f. Adib on 11/22/23.
+//
+
+import SwiftUI
+
+@main
+struct CupCakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
